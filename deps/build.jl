34ab347f0128a649a2223c38f0c2bec7348226ee
Maxim Egorov <maxgegorov@gmail.com>
2016-02-13 23:47:48 -0700

    added osx build

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-@unix_only begin
+@linux_only begin
 
 	download("https://github.com/cmansley/pomdp-solve/archive/master.zip", "pomdp-solve.zip")
 
@@ -6,6 +6,22 @@
 
 	rm("pomdp-solve.zip")
 	cd("pomdp-solve-master")
+
+	println("CONFIGURE");    tic(); run(`./configure`); toc()
+	println("MAKE");         tic(); run(`make`); toc()
+
+	cd(Pkg.dir("POMDPSolve", "deps"))
+end
+
+
+@osx_only begin
+
+	download("http://www.pomdp.org/code/pomdp-solve-5.4.tar.gz", "pomdp-solve-5.4.tar.gz")
+
+	println("UNZIPPING"); tic(); run(`tar -zxf pomdp-solve-5.4.tar.gz`); toc()
+
+	rm("pomdp-solve-5.4.tar.gz")
+	cd("pomdp-solve-5.4")
 
 	println("CONFIGURE");    tic(); run(`./configure`); toc()
 	println("MAKE");         tic(); run(`make`); toc()
