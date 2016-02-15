e33ebe81ed9fd11ca72cbd3820ec80d9518e23c6
Tim Wheeler <wheelert@stanford.edu>
2016-02-14 20:44:39 -0800

    moving source to pomdp-file page

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,10 @@
 @unix_only begin
 
-	download("https://github.com/cmansley/pomdp-solve/archive/master.zip", "pomdp-solve.zip")
-
-	println("UNZIPPING"); tic(); run(`unzip pomdp-solve.zip`); toc()
-
-	rm("pomdp-solve.zip")
-	cd("pomdp-solve-master")
+	download("http://www.pomdp.org/code/pomdp-solve-5.4.tar.gz", "pomdp-solve-5.4.tar.gz")
+	println("UNZIPPING"); tic(); run(`tar xvzf pomdp-solve-5.4.tar.gz`); toc()
+	rm("pomdp-solve-5.4.tar.gz")
+	
+	cd("pomdp-solve-5.4")
 
 	println("CONFIGURE");    tic(); run(`./configure`); toc()
 	println("MAKE");         tic(); run(`make`); toc()
